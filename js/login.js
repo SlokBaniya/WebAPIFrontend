9b2b9d18ddd6f5278b093e13686f02017ae914f3
SlokBaniya <slokbaniya1@gmail.com>
2019-06-28 09:15:55 +0545

    Create admin dash

---
+++ js/login.js
@@ -0,0 +1,39 @@
+window.onload = function() {
+
+    const button = document.getElementById('btnLogin');
+
+    button.addEventListener('click', function() {
+        const username = document.getElementById('username').value;
+        const password = document.getElementById('password').value;
+
+        // create a http request similar to postman
+
+        fetch('http://localhost:8000/api/user/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    username: username,
+                    password: password
+                })
+            })
+            .then(data => {
+                return data.json();
+            })
+            .then(json => {
+                if (json.status) {
+                    // localStorage.setItem('Token', json.accessToken);
+                    // window.location.href = 'dashboard.html'
+                    alert("login success");
+                } else {
+                    alert('user not found');
+                }
+            })
+            .catch(error => {
+                console.log(error)
+            })
+
+    })
+
+}
